ad67f14167bfb83cdc07adc106ad4b0ea2b9f4f3
Padmaja Unnam <padmaja.unnam@dcsg.com>
2021-06-09 11:40:50 -0500

    Initial Commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Padmaja Unnam on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
